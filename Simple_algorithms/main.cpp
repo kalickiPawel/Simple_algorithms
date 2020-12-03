2e646e536386e03643ff7c22aaa8ef97150e5a6e
Paweł <kalicki.paul@icloud.com>
2020-12-03 17:59:19 +0100

    Initial Commit

---
+++ Simple_algorithms/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Simple_algorithms
+//
+//  Created by Paweł Kalicki on 03/12/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
